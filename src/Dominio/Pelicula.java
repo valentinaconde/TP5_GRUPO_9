6bdf14319270ed5925e233207a83683df37e4608
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2025-04-30 12:05:10 -0300

    clase categorias y peliculas

---
+++ src/Dominio/Pelicula.java
@@ -0,0 +1,31 @@
+package Dominio;
+
+public class Pelicula {
+    private int id;
+    private String nombre;
+    private Categoria categoria; 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
