ea7d2d5f52251345ae5452710e8c0844cbe867f2
Matias-fabio <matias24_05.96@hotmail.com>
2025-05-07 14:57:08 -0300

    cambiar clase VentanaAgregar a JPanel, midifcaciones en Ventana

--- src/Dominio/Ventana.java
+++ src/Dominio/Ventana.java
@@ -1,7 +1,7 @@
 package Dominio;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+//import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,17 +15,17 @@ public class Ventana extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public Ventana() {
 		setTitle("Programa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout());
+        setContentPane(contentPane);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -35,17 +35,16 @@ public class Ventana extends JFrame {
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("Agregar");
 		mntmNewMenuItem.addActionListener(e -> {
-		    VentanaAgregar ventanaAgregar = new VentanaAgregar();
-		    ventanaAgregar.setVisible(true);
+			contentPane.removeAll();
+			contentPane.add(new VentanaAgregar(), BorderLayout.CENTER);
+			contentPane.revalidate();
+			contentPane.repaint();
 		});
 		mnNewMenu.add(mntmNewMenuItem);
 		
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Listar");
 		mnNewMenu.add(mntmNewMenuItem_1);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
 	}
 
 }
--- src/Dominio/VentanaAgregar.java
+++ src/Dominio/VentanaAgregar.java
@@ -1,8 +1,6 @@
 package Dominio;
 
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
@@ -13,10 +11,9 @@ import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.Font;
 
-public class VentanaAgregar extends JFrame {
+public class VentanaAgregar extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
 	private JTextField tfNombre;
 	private JLabel lblID;
     private static int ID=1;
@@ -26,54 +23,31 @@ public class VentanaAgregar extends JFrame {
 	private Categoria categoria;
 	
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaAgregar frame = new VentanaAgregar();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public VentanaAgregar() {
-		setTitle("Programa");
-		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		
+		
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("ID");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel.setBounds(67, 40, 46, 14);
-		contentPane.add(lblNewLabel);
+		add(lblNewLabel);
 		
 		JLabel lblNombre = new JLabel("Nombre");
 		lblNombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNombre.setBounds(66, 91, 69, 14);
-		contentPane.add(lblNombre);
+		add(lblNombre);
 		
 		tfNombre = new JTextField();
 		tfNombre.setBounds(184, 90, 125, 20);
-		contentPane.add(tfNombre);
+		add(tfNombre);
 		tfNombre.setColumns(10);
 		
 		JLabel lblGenero = new JLabel("Genero");
 		lblGenero.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblGenero.setBounds(67, 136, 46, 14);
-		contentPane.add(lblGenero);
+		add(lblGenero);
 		
 		 cbGenero = new JComboBox<String>();
 	        cbGenero.setModel(new DefaultComboBoxModel<String>(new String[] {
@@ -82,16 +56,16 @@ public class VentanaAgregar extends JFrame {
 		
 		
 		cbGenero.setBounds(184, 132, 125, 22);
-		contentPane.add(cbGenero);
+		add(cbGenero);
 		
 		JButton btnAceptar = new JButton("Aceptar");
 		btnAceptar.setBounds(67, 172, 135, 23);
 		btnAceptar.addActionListener(e -> accionAceptar());
-		contentPane.add(btnAceptar);
+		add(btnAceptar);
 		
 		lblID = new JLabel("");
 		lblID.setBounds(184, 42, 46, 14);
-		contentPane.add(lblID);
+		add(lblID);
 		lblID.setText("" + ID);
 		
 		
